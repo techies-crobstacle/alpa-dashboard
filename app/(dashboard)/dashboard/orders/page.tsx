a33c195c35ffe7fbaf257a9f167c4d808f6db695
techies-crobstacle <techies@crobstacle.com>
2026-02-27 18:33:07 +0530

    Fixed the skeleton loading

--- app/(dashboard)/dashboard/orders/page.tsx
+++ app/(dashboard)/dashboard/orders/page.tsx
@@ -14,7 +14,7 @@
  
 // import { toast } from "sonner";
 
-// const BASE_URL = "http://127.0.0.1:5000";
+// const BASE_URL = "https://alpa-be.onrender.com";
 
 // // --- API HELPERS ---
 // const getAuthHeaders = () => {
@@ -434,7 +434,7 @@ import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@
  
 import { toast } from "sonner";
 
-const BASE_URL = "http://127.0.0.1:5000";
+const BASE_URL = "https://alpa-be.onrender.com";
 
 // --- API HELPERS ---
 const getAuthHeaders = () => {
